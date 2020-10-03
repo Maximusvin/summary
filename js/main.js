92cd379dbe117fe2a383ce646193f96362b95f28
Maximusvin <Maximusvin@gmail.com>
2020-10-03 23:33:52 +0300

    g

--- js/main.js
+++ js/main.js
@@ -1,34 +1,15 @@
-"use strict";
+'use strict';
 
 const refs = {
-  burger: document.querySelector("#burger"),
-  menu: document.querySelector(".menu"),
+  burger: document.querySelector('#burger'),
+  menu: document.querySelector('.menu'),
 };
 
-refs.burger.addEventListener("click", toggleBurger);
+refs.burger.addEventListener('click', toggleBurger);
 
 function toggleBurger(event) {
   event.preventDefault();
-  event.currentTarget.classList.toggle("active");
-  refs.menu.classList.toggle("active");
-  document.body.classList.toggle("lock");
+  event.currentTarget.classList.toggle('active');
+  refs.menu.classList.toggle('active');
+  document.body.classList.toggle('lock');
 }
-
-// ===================================
-const timer = {
-  start() {
-    const startTime = Date.now();
-
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      console.log(pad(deltaTime));
-    }, 1000);
-  },
-};
-
-function pad(value) {
-  return String(value / 1000).padStart(2, "0");
-}
-
-// timer.start();
