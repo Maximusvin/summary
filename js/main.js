bfb602bc1c043008547241e44831fef057f50e5f
Maximusvin <Maximusvin@gmail.com>
2020-10-04 22:33:31 +0300

    e

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@
 const refs = {
   burger: document.querySelector('#burger'),
   menu: document.querySelector('.menu'),
+  menuWrapp: document.querySelector('.menu-wrapp'),
 };
 
 refs.burger.addEventListener('click', toggleBurger);
-refs.menu.addEventListener('click', closeBurger);
+refs.menuWrapp.addEventListener('click', closeBurger);
 
 function toggleBurger(event) {
   event.preventDefault();
   event.currentTarget.classList.toggle('active');
+  refs.menuWrapp.classList.toggle('active');
   refs.menu.classList.toggle('active');
   document.body.classList.toggle('lock');
 }
@@ -18,5 +20,6 @@ function toggleBurger(event) {
 function closeBurger(event) {
   refs.burger.classList.remove('active');
   refs.menu.classList.remove('active');
+  refs.menuWrapp.classList.remove('active');
   document.body.classList.remove('lock');
 }
