78a8eba10625b80a700e23a43e4e6d782d8aecd5
Maximusvin <Maximusvin@gmail.com>
2020-10-12 21:14:20 +0300

    d

--- js/main.js
+++ js/main.js
@@ -43,3 +43,26 @@ $(document).ready(function () {
     return false;
   });
 });
+
+
+// SLIDER
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters  
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
